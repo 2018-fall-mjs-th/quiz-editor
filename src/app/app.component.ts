ca66f0b912d2df78c008084dce38eaee34aaca6e
Sylvie <sschuresko@madisoncollege.edu>
2018-10-25 18:26:04 -0500

    adding more Angular binding types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+  dumb = 'foo';
+  titleColorDanger = this.dumb === 'foo' ? true: false;
+  titleBackgroundColorDanger = false;
+  headingTwoBackgroundColor = this.dumb === 'foo' ? "lightcoral" : "lightblue";
+  imageWidth = 200;
+
+  private increaseImageWidth = () => this.imageWidth *= 1.5;
+
 }
