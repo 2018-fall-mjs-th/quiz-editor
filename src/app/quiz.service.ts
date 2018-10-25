65d63d710a55cf963212d3fcc0e79274726ae8b4
Mehmet Asal <35569568+msasal@users.noreply.github.com>
2018-10-25 18:56:42 -0500

    added getQuizzes to the quiz service

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  getQuizzes() {
+    return [
+      { name: "Quiz 1", numberOfQuestions:6 }
+      , { name: "Quiz 2", numberOfQuestions:0 }
+      , { name: "Quiz 3", numberOfQuestions:16 }
+    ];
+  }
 }
