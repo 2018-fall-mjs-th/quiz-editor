c346354c21c404efb05c8c7e640ee6495fadaa69
Mary Cummins <marykcu@gmail.com>
2018-10-25 18:56:23 -0500

    added getQuizzes() method to the quiz service

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  // returns an array of quiz objects
+  getQuizzes() {
+    return [
+      { name: "Quiz 1", numberOfQuestions: 6 }
+      , { name: "Quiz 2", numberOfQuestions: 0 }
+      , { name: "Quiz 3", numberOfQuestions: 16 }
+    ];
+  }
 }
