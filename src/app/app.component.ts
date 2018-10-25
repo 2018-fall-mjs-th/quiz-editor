366c7096c73b0fd68b5453a75c96cc85bd4ef46b
Mahlet Amare <35571150+Mahletmek@users.noreply.github.com>
2018-10-25 18:46:44 -0500

    modifying html for quiz-editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+
+  dumb = "foo123";
+
+  titleColorDanger = this.dumb === 'foo' ? true : false;
+  titleBackgroundColor = false;
+
+  headingtwoBackgroundColor = this.dumb === 'foo' ? 'Red' : 'Blue';
+
+  imageWidth: number = 300; 
+
+ increaseImageWidth = () => {this.imageWidth *=1.5};
 }
