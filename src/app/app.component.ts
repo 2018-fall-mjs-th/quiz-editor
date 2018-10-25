f00a4678de5de6a7d248610b607cb0ad6eec90d5
EAlbee <EAlbee@madisoncollege.edu>
2018-10-25 18:25:55 -0500

    A few more Angular Binding types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,10 @@ export class AppComponent {
   dumb: string = "foo";
   titleColorDanger = this.dumb === 'foo' ? true:false;
   titleBackgroundColorDanger = false;
+
+  headingToBackgroundColor = this.dumb === 'foo' ? 'Red' : 'Blue';
+
+  imageWidth: number = 300;
+
+   increaseImageWidth = () => { this.imageWidth *= 1.5};
 }
