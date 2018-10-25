8cf8b1c6c9ec497dab4bf3d33a2a3933f6be6805
drewburns1314 <37885864+drewburns1314@users.noreply.github.com>
2018-10-25 18:55:10 -0500

    fixed quiz list added

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  getQuizzes() {
+    return [
+      { name: "Quiz 1", numberofQuestions: 6}      
+      , { name: "Quiz 2", numberofQuestions: 4}      
+      , { name: "Quiz 3", numberofQuestions: 8}
+    ];
+  }
 }
