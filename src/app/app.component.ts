ab15a5a0817ff3f73e28d15e553d9b910b501b1f
Robert Collier <35576077+Sescheraun@users.noreply.github.com>
2018-10-25 18:53:00 -0500

    cleaning my commit log for actual work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { getLocaleDateFormat, FormatWidth } from '@angular/common';
 
 @Component({
+  
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+  , styleUrls: ['./app.component.css']
+    
+
 })
 export class AppComponent {
   title = 'quiz-editor';
+
+  titleTextDanger = true;
+  titleBackDanger = false;
+
 }
