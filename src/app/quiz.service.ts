d583462ca80d95e17d5c77f0189febe1094ced7a
Isaac Lemke <cpu.ikemike@gmail.com>
2018-10-25 18:56:26 -0500

    getQuizzes() Method Added to Quiz Service

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,16 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  /**
+   * Get an array of quizzes via a REST service and return as JSON
+   * @returns String - A JSON array 
+   */
+  getQuizzes() {
+    return [
+      { name: "Quiz 1", numberOfQuestions: 6 }
+      , { name: "Quiz 2", numberOfQuestions: 0 }
+      , { name: "Quiz 3", numberOfQuestions: 16 }
+    ];
+  }
 }
