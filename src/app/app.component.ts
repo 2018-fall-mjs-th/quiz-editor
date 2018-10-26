e2dafcd32bd873ae0c07caf5fefcbb88c508b0fa
gbitz <gbitzer@madisoncollege.edu>
2018-10-25 19:51:49 -0500

    build initial quiz list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import { QuizService } from './quiz.service';
 })
 export class AppComponent {
   
+  quizzes = [];
   constructor (private quizSvc: QuizService) {
-    console.log(this.quizSvc.getQuizzes());
+    // console.log(this.quizSvc.getQuizzes());
+    this.quizzes = this.quizSvc.getQuizzes();
   }
   
   title = 'quiz-editor';
