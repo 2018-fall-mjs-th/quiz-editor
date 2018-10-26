fd9dbe66a902d2775efa673470754902808d985c
Paul Cotts <pdcotts@users.noreply.github.com>
2018-10-25 19:38:09 -0500

    Take dependency on QuizService
    Take a Test on Consle.out . . .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
+
 export class AppComponent {
+
+  constructor (private quizSvc: QuizService) {
+    console.log(this.quizSvc.getQuizzes());
+  }
+
   title = 'quiz-editor';
 
   dumb: string = "foo123";
--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class QuizService {
 
-  constructor() { }
+	constructor() { }
+
+	getQuizzes() {
+		return [
+			{ name: "Quiz 1", numberOfQuestions: 6 }
+			, { name: "Quiz 2", numberOfQuestions: 0 }
+			, { name: "Quiz 3", numberOfQuestions: 16 }
+		]
+	}
+
 }
