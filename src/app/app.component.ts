59bd55aed9c9c22a6bf9622c2713bc1272d85a96
Sylvie <sschuresko@madisoncollege.edu>
2018-10-25 19:51:47 -0500

    Built initial quiz list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ import { QuizService } from './quiz.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  quizzes = [];
   constructor (private quizSvc: QuizService) {
-    console.log(this.quizSvc.getQuizzes());
+    //console.log(this.quizSvc.getQuizzes());
+    this.quizzes = this.quizSvc.getQuizzes();
   }
   title = 'quiz-editor';
   dumb = 'foo';
