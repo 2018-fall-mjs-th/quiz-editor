3fbb1211a0ddf4ff1bc2611ca1004910505fbc5b
EAlbee <EAlbee@madisoncollege.edu>
2018-10-25 19:51:47 -0500

    Built initial quiz list...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,11 @@ import { QuizService } from './quiz.service';
 })
 export class AppComponent {
   
+  quizzes = [];
+
   constructor (private quizSvc: QuizService) {
-    console.log(this.quizSvc.getQuizzes());
+    //console.log(this.quizSvc.getQuizzes());
+    this.quizzes = this.quizSvc.getQuizzes();
   }
 
   title = 'quiz-editor';
