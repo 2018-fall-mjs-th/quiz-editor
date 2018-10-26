9902fd22525f2809ebaf35660d48df4dcb06f3de
jlemke <serikosx@gmail.com>
2018-10-25 19:37:58 -0500

    Initial commit and dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private quizSvc: QuizService) {
+    console.log(this.quizSvc.getQuizzes());
+  }
+
   title = 'quiz-editor';
+  
+  dumb: string = "foo123";
+
+  titleColorDanger = true;
+  titleColorDangerBackground = this.dumb === 'foo' ? true : false;
+
+  headingTwoBackgroundColor = this.dumb === 'foo' ? 'Red' : 'Blue';
+
+  imageWidth: number = 100;
+
+  increaseImageWidth = () => {
+    this.imageWidth *= 1.5;
+  };
 }
---
+++ src/app/quiz.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+
+  constructor() { }
+
+  getQuizzes() {
+    return [
+      { name: "Quiz 1", numberOfQuestions: 6 },
+      { name: "Quiz 2", numberOfQuestions: 0 },
+      { name: "Quiz 3", numberOfQuestions: 16}
+    ]
+  }
+}
