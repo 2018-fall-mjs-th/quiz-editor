aeeb87aa6de65c4ef9420bc136d5e47a65a71027
Mahlet Amare <35571150+Mahletmek@users.noreply.github.com>
2018-11-01 18:34:01 -0500

    Calling webservice with observable
    great!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,17 @@ import { QuizService } from './quiz.service';
 })
 export class AppComponent {
 
-  quizzes = [];
+  quizzes: any = [];
 
   constructor (private quizSvc: QuizService) {
   
    }
   
   ngOnInit() {
-    this.quizzes = this.quizSvc.getQuizzes();
+    this.quizSvc.getQuizzes().subscribe (
+      data => this.quizzes = data
+    );
+   
   }
 
   title = 'quiz-editor';
