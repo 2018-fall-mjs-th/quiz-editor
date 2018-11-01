fecb27b2d0c4db7663f5f8079bf2325e40874179
Kevin Selm <35573154+kcselm@users.noreply.github.com>
2018-11-01 18:01:20 -0500

    ngOnInit()
    Discovering life cylce hooks.  Init

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -7,11 +7,15 @@ export class QuizService {
 
   constructor() { }
 
+  ngOnInit() {
+    this.quizzes = this.quizSVC.getQuizzes()
+  }
+
   getQuizzes() {
     return [
       {name: "Quiz 1", numberOfQuestions: 6}
       , {name: "Quiz 2", numberOfQuestions: 0}
       , {name: "Quiz 3", numberOfQuestions: 10}
-    ];
+    ]
   }
 }
