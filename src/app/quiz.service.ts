d45a920a5d103e9016282cd3589d37c313635843
EAlbee <albee_eric@hotmail.com>
2018-11-01 18:21:32 -0500

    Took dependency on HTTPClient
    and added it to oven

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
 
   getQuizzes() {
     return [
-      { name: "Quiz 1", numberOfQuestions: 6 }
+    /*   { name: "Quiz 1", numberOfQuestions: 6 }
       , { name: "Quiz 2", numberOfQuestions: 0 }
-      , { name: "Quiz 3", numberOfQuestions: 16 }
+      , { name: "Quiz 3", numberOfQuestions: 16 } */
+      //https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz
+    
     ];
   }
 }
