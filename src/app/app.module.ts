86aecd858c262bc45b3918d68d94c867beab4f63
David <43051959+dnoo27@users.noreply.github.com>
2018-11-01 18:19:11 -0500

    added some things to constructor
    added dependency to constructor, and then imported http thingy, and then added httpClientModule to imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
     , FormsModule
+    , HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
