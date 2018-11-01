8272824055736ae1b8e4d8abc227d8e3cb4a3631
Mahlet Amare <35571150+Mahletmek@users.noreply.github.com>
2018-11-01 18:23:51 -0500

    set up the oven

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -11,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
     , FormsModule
+    , HttpClientModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
