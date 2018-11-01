4d0e02ee995b756b88262e0417a534a1bc46fcd5
Ben Castro <35582847+bcastro1991@users.noreply.github.com>
2018-11-01 18:16:48 -0500

    Updated quiz.services.ts to use Http requested
    app is not running....

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
+
+  // https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz
 
   getQuizzes() {
     return [
