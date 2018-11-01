98c41a5c4076aa44d514c0c458373d343325cf22
Mehmet Asal <35569568+msasal@users.noreply.github.com>
2018-11-01 18:21:50 -0500

    took dependency on http module

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
 
   getQuizzes() {
+
+    // https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz
+
+
     return [
       { name: "Quiz 1", numberOfQuestions:6 }
       , { name: "Quiz 2", numberOfQuestions:0 }
