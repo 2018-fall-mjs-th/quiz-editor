4bd1c614377eeea3178b9d2dcb378cda51981bc0
Mahlet Amare <35571150+Mahletmek@users.noreply.github.com>
2018-11-01 19:48:29 -0500

    Promise await demo
    most promise

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { resolve } from 'dns';
 
 
 @Injectable({
@@ -19,5 +20,15 @@ export class QuizService {
     //   , { name: "Quiz 2", numberOfQuestions: 0 }
     //   , { name: "Quiz 3", numberOfQuestions: 16 }
     // ];
+  }
+
+  getNumberOfQuizzes(succeed: boolean): Promise<number> {
+   
+    let p = new Promise<number>(
+       (resolve, reject) => 
+         succeed ? resolve(42) : reject("failed!")
+      );
+
+      return p;
   }
 }
