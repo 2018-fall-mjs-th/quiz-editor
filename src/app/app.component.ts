c23486c508361d62455ca86e84f94c82992c7080
Isaac Lemke <cpu.ikemike@gmail.com>
2018-11-08 20:49:51 -0600

    Working on Save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,11 @@ interface questionDisplay {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quizzes: quizDisplay[] = [];
+  quizzes: quizDisplay[] = [];          // UI display of quizzes (not necessarily committed information )
   responseError: boolean = false;
   selectedQuiz = undefined;
+  savedQuizzes: quizDisplay[] = [];     // After save is clicked
+  selectedQuestions = undefined;
 
   constructor (private quizSvc: QuizService) { }
 
@@ -29,23 +31,33 @@ export class AppComponent {
     this.quizSvc.getQuizzes('Isaac').subscribe(data => {
       console.log(data);
       this.quizzes = <quizDisplay[]> data;
+      this.savedQuizzes = <quizDisplay[]> data;
     }, err => {
       console.log('There was an error with the request');
       console.log(err);
       this.responseError = true;
     })
     
-    //this.quizSvc.getQuizzes('Isaac').subscribe(data => this.quizzes = <quizDisplay[]> data);
-    
-    
+    //this.quizSvc.getQuizzes('Isaac').subscribe(data => { this.quizzes = <quizDisplay[]> data);
+  
   }
 
   public selectQuiz(q) {
+
+    // Reset the previous information if it wasn't saved
+    if (this.selectedQuiz) {
+      this.selectedQuiz.questions = this.selectedQuestions;
+      this.updateQuestionCount(this.selectedQuiz);
+    }   
+
     this.selectedQuiz = q;
+    this.selectedQuestions = q.questions;
+    this.updateQuestionCount(q);
   }
 
   public addNewQuiz() {
-    let newQuiz = <quizDisplay> { name: '', numberQuestions: 0, questions: [] };
+    let questions = [];
+    let newQuiz = <quizDisplay> { name: 'New Quiz', numberQuestions: questions.length, questions: questions };
     this.quizzes = [...this.quizzes, newQuiz];
     this.selectedQuiz = newQuiz;
   }
@@ -53,13 +65,30 @@ export class AppComponent {
   public addNewQuestion() {
     let newQuestion = <questionDisplay> { name: "New Question" };
     this.selectedQuiz.questions = [...this.selectedQuiz.questions, newQuestion];
+    this.updateQuestionCount(this.selectedQuiz);
   }
 
   public deleteQuestion(questionToRemove) {
     this.selectedQuiz.questions = this.selectedQuiz.questions.filter(aQuestion => {
       return questionToRemove != aQuestion;
     });
+    this.updateQuestionCount(this.selectedQuiz);
   }
+
+  public saveQuizAndQuestions() {
+    this.selectedQuiz.questions = this.selectedQuestions;
+    
+  }
+
+  public cancelQuizEdits() {
+    this.selectedQuiz.questions = this.selectedQuestions;
+  }
+
+  public updateQuestionCount(aQuiz) {
+    aQuiz.numberQuestions = aQuiz.questions.length;
+  }
+
+  
 
   public myPromise() {
     console.log(this.quizSvc.getNumberOfQuizzes);
