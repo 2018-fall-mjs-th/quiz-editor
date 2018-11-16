8d787a5d9ff980649e2699e41fa23104308d780a
EAlbee <albee_eric@hotmail.com>
2018-11-15 20:02:32 -0600

    Animation working
    sort of...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { QuizService } from './quiz.service';
+import {
+  trigger
+  , style
+  , animate
+  , transition
+  , keyframes
+} from '@angular/animations';
 
 
 interface quizDisplay {
@@ -12,7 +19,29 @@ interface quizDisplay {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('detailsFromLeft', [
+      transition('leftPosition => finalPosition', [
+        animate('300ms', keyframes([
+          style({ left: '-30px', offset: 0.0 }),
+          style({ left: '-20px', offset: 0.25 }),
+          style({ left: '-10px', offset: 0.5 }),
+          style({ left: '-5px', offset: 0.75 }),
+          style({ left: '0px', offset: 1.0 })
+        ]))
+      ]),
+    ]),
+    trigger('pulseSaveCancelButtons', [
+      transition('nothingToSave => somethingToSave', [
+        animate('400ms', keyframes([
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 0.0 }),
+          style({ transform: 'scale(1.2)', 'transform-origin': 'top left', offset: 0.5 }),
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
 
@@ -62,6 +91,8 @@ export class AppComponent {
 
     console.log(this.numberOfChangedQuizzes);
     //this.numberOfChangedQuizzes = 75;
+
+    this.detailsAnimationState = "finalPosition";
   }
 
   removeQuestion(selectedQuiz, selectedQuestion) {
@@ -78,7 +109,7 @@ export class AppComponent {
     return changedQuizzes.length;
   }
 
-
+  detailsAnimationState = "leftPosition";
 
   // Learning promises functions below...
 
