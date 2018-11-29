b395b9801765d4a179e0aebe3c897ea1c54c95f8
Tom Steele <tsteele@madisoncollege.edu>
2018-11-29 16:50:37 -0600

    Cleaned up for class snippet...

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -30,53 +30,26 @@ export class QuizService {
     return p;
   }
 
-  saveQuizzes(changedQuizzes: any[]) {
-
-    //let headers = new Headers({ 'Content-Type': 'application/json' });
-    //let options = new RequestOptions({ headers: headers });
-    // let headers = new Headers()
-    //   .set("X-Sas-Token", "sig=WLBiqg_HC_FaEVqTUL4EeujDBQRQT9oL3R5uo1kIB1g")
-    //   ;
-
-    // headers.set('Content-Type', 'application/json')
+  saveQuizzes(changedQuizzes: any[], newQuizzes: any[] = []) {
 
     let h = new HttpHeaders({
       'Content-Type': 'application/json'
-      //, 'X-Sas-Token': 'sig=WLBiqg_HC_FaEVqTUL4EeujDBQRQT9oL3R5uo1kIB1g'
       , 'X-Sas-Token': 'sig=K2WE6NQPtyoV6ke5hwPEaEaW52fgvyFWUeCEdPJls1s'
     });
 
-    //h.append('Content-Type', 'application/json');
-    //h.append('X-Sas-Token', 'sig=WLBiqg_HC_FaEVqTUL4EeujDBQRQT9oL3R5uo1kIB1g');
-    console.log(h);
+    //console.log(h);
 
     return this.builtInAngularHttpClient.post(
-      //'https://modern-js.azurewebsites.net/add-quizzes-proxy'
       'https://modern-js.azurewebsites.net/save-quizzes-proxy'
       , JSON.stringify(
         {
           "changedQuizzes": changedQuizzes
-          , "newQuizzes": []
-          // , "newQuizzes": [{ 
-          //     "quizName": "Foo"
-          //     , "quizQuestions": [
-          //         "qOne"
-          //         , "qTwo"
-          //     ]
-          // }
-          // , {
-          //     "quizName": "Bar"
-          //     , "quizQuestions": [
-          //         "cat"
-          //         , "dog"
-          //         , "parrot"
-          //     ]
-          // }]
+          , "newQuizzes": newQuizzes
         }
       )
-    , {
-      headers: h
-    }
+      , {
+        headers: h
+      }
     );
   }
 }
