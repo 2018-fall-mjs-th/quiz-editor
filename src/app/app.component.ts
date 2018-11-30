a96010248171921d86aa2b16759e5528dbf25981
Ben Castro <35582847+bcastro1991@users.noreply.github.com>
2018-11-29 19:52:47 -0600

    Successfully was able to complete week 13 slack n tell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -115,7 +115,13 @@ export class AppComponent implements OnInit {
       || x.naiveQuestionsChecksum !== x.questions.map(y => y.name).join('~'))
     ); 
 
-    this.quizSvc.saveQuizzes(changedQuizzes).subscribe(
+    const newQuizzes  = this.quizzes
+    .filter(x => x.originalName === 'New Untitled Quiz')
+    .map(x => ({'quizName' : x.name 
+    , 'quizQuestions' : x.questions
+      .map(y=>y.name)}));
+
+    this.quizSvc.saveQuizzes(changedQuizzes, newQuizzes).subscribe(
       data => console.log(data)
       , error => console.log(error)
     );
